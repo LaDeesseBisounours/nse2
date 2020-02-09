85d7c75ffeef1b7599c164c04a2285c84cb4e83b
Maxime Mehio <maxime.mehio@epita.fr>
2020-02-09 19:18:37 +0100

    euh pk ca marche pas

--- src/test_idt.cc
+++ src/test_idt.cc
@@ -5,14 +5,17 @@
 
 unsigned long get_addr_idt (void)
 {
+    printf("skdkflsjdf");
     unsigned char idtr[6];
     unsigned long idt;
     __asm__ volatile ("sidt %0": "=m" (idtr));
     idt = *((unsigned long *) &idtr[2]);
+    printf("skdkflsjdf");
     return(idt);
 }
 
 int main(void) {
+    printf("skdkflsjdf");
     unsigned long idtp = get_addr_idt();
     printf("unsigned %lu\n", idtp);
     printf("hexa %lx\n", idtp);
